76a89951f1fef67f625e57dcad0d9dea0e6e9e3d
Justine Paralejas <paralejasjustine15@gmail.com>
2024-11-28 01:07:21 +0800

    Naming convention.

--- server/controllers/api/file.js
+++ server/controllers/api/file.js
@@ -8,31 +8,27 @@ const bucket = storage.bucket("filespace-bucket");
 
 const handleUploadFile = async (req, res) => {
   const { id } = req.user;
-  const files = req.files;
+  const _files = req.files;
 
-  if (!files || files.length === 0) {
-    return res
-      .status(400)
-      .send("No file uploaded, there's no files to upload.");
-  }
+  if (!_files || _files.length === 0)
+    return res.status(400).send("No file/s to upload.");
 
   try {
-    const filesToUpload = Array.isArray(files) ? files : [files];
+    const files = Array.isArray(_files) ? files : [_files];
 
-    const uploadPromises = filesToUpload.map(async (file) => {
+    const fileUploads = files.map(async (file) => {
       const fileName = `${id}/${file.originalname}`;
 
-      const fileUpload = bucket.file(fileName);
+      const newFile = bucket.file(fileName);
 
-      await fileUpload.save(file.buffer, {
+      await newFile.save(file.buffer, {
         resumable: false,
         metadata: {
           contentType: file.mimetype,
-          owner: id,
         },
       });
 
-      await fileUpload.setMetadata({
+      await newFile.setMetadata({
         metadata: {
           owner: id,
         },
@@ -41,9 +37,9 @@ const handleUploadFile = async (req, res) => {
       return `File uploaded successfully: ${fileName}`;
     });
 
-    const uploadResults = await Promise.all(uploadPromises);
+    const fileUploadResults = await Promise.all(fileUploads);
 
-    return res.status(200).send(uploadResults);
+    return res.status(200).send(fileUploadResults);
   } catch (error) {
     console.error(error);
     return res.status(500).send("Internal Server Error");
